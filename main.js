c4b2995d427f5ef32ea3fa0f11511a2bec75e7c7
Mohamed Bechir Mejri <mohamed.bechir.mejri.dev@gmail.com>
2021-05-17 08:49:52 +0100

    shake container instead of sketcharea

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const sketchArea = document.getElementById("sketch-area");
 const chosenColor = document.getElementById("chosen-color");
 const resetButton = document.getElementById("reset");
 const numOfSquaresButton = document.getElementById("number-of-squares");
+const container = document.getElementById("container");
 
 function createSquares() {
   sketchArea.style.gridTemplateColumns = `repeat(${numOfSquares}, 1fr)`;
@@ -21,11 +22,11 @@ function createSquares() {
 
 sketchArea.addEventListener("transitionend", () =>
   // TODO fix this
-  sketchArea.classList.toggle("shaken")
+  container.classList.toggle("shaken")
 );
 
 resetButton.addEventListener("click", () => {
-  sketchArea.classList.toggle("shaken");
+  container.classList.toggle("shaken");
   sketchArea.innerHTML = "";
   createSquares();
 });
