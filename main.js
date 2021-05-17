14023fb0c732e3632b498344897a3427480ddd4f
Mohamed Bechir Mejri <mohamed.bechir.mejri.dev@gmail.com>
2021-05-17 08:36:28 +0100

    style:

--- main.js
+++ main.js
@@ -1,31 +1,35 @@
 let numOfSquares = 16;
 const sketchArea = document.getElementById("sketch-area");
 const chosenColor = document.getElementById("chosen-color");
+const resetButton = document.getElementById("reset");
+const numOfSquaresButton = document.getElementById("number-of-squares");
 
 function createSquares() {
   for (let i = 0; i < numOfSquares * numOfSquares; i++) {
     const square = document.createElement("div");
+
     square.addEventListener(
       "mouseover",
       () => (square.style.backgroundColor = chosenColor.value)
     );
+
     sketchArea.appendChild(square);
   }
+
   sketchArea.style.gridTemplateColumns = `repeat(${numOfSquares}, 1fr)`;
   sketchArea.style.gridTemplateRows = `repeat(${numOfSquares}, 1fr)`;
 }
-createSquares();
-
-const resetButton = document.getElementById("reset");
-const numOfSquaresButton = document.getElementById("number-of-squares");
 
 sketchArea.addEventListener("transitionend", () =>
   // TODO fix this
-  sketchArea.classList.remove("shaken")
+  sketchArea.classList.toggle("shaken")
 );
 
 resetButton.addEventListener("click", () => {
-  sketchArea.classList.add("shaken");
+  sketchArea.classList.toggle("shaken");
   sketchArea.innerHTML = "";
+
   createSquares();
 });
+
+createSquares();
